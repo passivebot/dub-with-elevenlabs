a0361185b399d5627d6e6a56b1c7cb7e14ffd2fc
Harminder Singh Nijjar <110620707+harmindersinghnijjar@users.noreply.github.com>
2023-11-10 11:24:20 -0800

    Add files via upload

--- dub-with-elevenlabs/background.js
+++ dub-with-elevenlabs/background.js
@@ -62,110 +62,83 @@ function injectDataIntoPage(videoDetails) {
         waitForElement("//*[contains(text(),'Youtube')]", true, (youtubeOption) => {
             youtubeOption.click();
 
-            // Print the video name and URL to the console
-            console.log(`Video name: ${videoDetails.name}`);
-            console.log(`Video URL: ${videoDetails.url}`);
+            // Select the parent div element by its class name "mb-4 w-full"
+            const parentDiv = document.querySelector('.mb-4.w-full');
+            if (parentDiv) {
+                const inputElement = parentDiv.querySelector('input');
+                if (inputElement) {
+                    inputElement.value = videoDetails.name;
+                    inputElement.style.border = '2px solid red';
+                    inputElement.dispatchEvent(new Event('input', {
+                        bubbles: true
+                    }));
+                }
+            }
 
-            // Add a delay (e.g., 2 seconds) before continuing
-            setTimeout(() => {
-                // Select the parent div element by its class name "mb-4 w-full"
-                const parentDiv = document.querySelector('.mb-4.w-full');
+            const videoNameInput = document.querySelector("input[placeholder='Untitled']");
+            if (videoNameInput) {
+                videoNameInput.value = videoDetails.name;
+                videoNameInput.style.border = '2px solid red';
+                videoNameInput.dispatchEvent(new Event('input', {
+                    bubbles: true
+                }));
+            }
 
-                if (parentDiv) {
-                    // Find the input element inside the parent div
-                    const inputElement = parentDiv.querySelector('input');
+            const videoUrlInput = document.querySelector("input[placeholder='https://www.youtube.com/watch?v=XYLgwxbwEb8']");
+            if (videoUrlInput) {
+                videoUrlInput.value = videoDetails.url;
+                videoUrlInput.style.border = '2px solid red';
+                videoUrlInput.dispatchEvent(new Event('input', {
+                    bubbles: true
+                }));
+            }
 
-                    if (inputElement) {
-                        // You can access its value property
-                        const inputValue = inputElement.value;
+            function selectButtonWithHighestIdentifier() {
+                const buttons = document.querySelectorAll('button[id^="popover-trigger-"]');
+                let highestIdentifier = '';
+                let highestButtonElement = null;
 
-                        // Modify the input element's value
-                        const text = `${videoDetails.name}`;
-                        const event = new Event('input', {
-                            bubbles: true
-                        });
-                        event.simulated = true;
-                        inputElement.value = text;
-
-                        // Dispatch the 'input' event to trigger any associated event listeners
-                        inputElement.dispatchEvent(event);
-
-                        // Highlight the input field
-                        inputElement.style.border = '2px solid red';
-                    } else {
-                        console.error('Input element not found inside the parent div.');
+                buttons.forEach(button => {
+                    const idParts = button.id.split(':');
+                    const identifier = idParts[idParts.length - 2];
+                    if (identifier > highestIdentifier) {
+                        highestIdentifier = identifier;
+                        highestButtonElement = button;
                     }
-                } else {
-                    console.error('Parent div element not found.');
-                }
+                });
 
-                // Locate by placeholder text
-                const videoNameInput = document.querySelector("text-input[placeholder='Untitled']");
+                return highestButtonElement;
+            }
 
-                if (videoNameInput) {
-                    // Highlight the input field
-                    videoNameInput.style.border = '2px solid red';
-                    const text = `${videoDetails.name}`;
-                    const event = new Event('input', {
-                        bubbles: true
-                    });
-                    event.simulated = true;
-                    videoNameInput.value = text;
-                    videoNameInput.dispatchEvent(event);
-                }
+            highestButtonElement = selectButtonWithHighestIdentifier();
+            if (highestButtonElement) {
+                highestButtonElement.style.border = '2px solid red';
+                highestButtonElement.click();
 
-                // Locate by placeholder text
-                const videoUrlInput = document.querySelector("input[placeholder='https://www.youtube.com/watch?v=XYLgwxbwEb8']");
+                setTimeout(() => {
+                    const spanElements = document.querySelectorAll("span");
+                    spanElements.forEach((span) => {
+                        if (span.textContent.includes('Hindi')) {
+                            span.click();
+                        }
+                    }, 2000); // 2000 milliseconds = 2 seconds
+                });
 
-                if (videoUrlInput) {
-                    // Highlight the input field
-                    videoUrlInput.style.border = '2px solid red';
-                    const text = `${videoDetails.url}`;
-                    const event = new Event('input', {
-                        bubbles: true
-                    });
-                    event.simulated = true;
-                    videoUrlInput.value = text;
-                    videoUrlInput.dispatchEvent(event);
-                } else {
-                    console.error('Video URL input element not found.');
-                }
+                setTimeout(() => {
+                    // Wait for 10 seconds before clicking the 'Create Dub' button so that tokens are calculated
+                    const createDubButton = document.querySelector(".btn.btn-primary.btn-md.btn-normal");
 
-                // Select the button element with the specific ID
-                const buttonElement = document.querySelector("#popover-trigger-\\:r11\\:");
+                    if (createDubButton) {
+                        // Click on the 'Create Dub' button
+                        createDubButton.click();
+                        console.log('Dub created successfully.');
+                    } else {
+                        console.error('Create Dub button not found.');
+                    }
+                }, 10000);
 
-                if (buttonElement) {
-                    // Highlight the input field
-                    buttonElement.style.border = '2px solid red';
-                    buttonElement.click();
-                    // Add a delay (e.g., 2 seconds) before continuing
-                    setTimeout(() => {
-                        // Locate and click the "Hindi" element based on its text content
-                        const spanElements = document.querySelectorAll("span");
-                        spanElements.forEach((span) => {
-                            if (span.textContent.includes('Hindi')) {
-                                span.click();
-                            }
-                        });
-                    }, 2000); // 2-second delay before clicking Hindi
-                } else {
-                    console.error('Button element not found.');
-                }
-            }, 2000); // 2-second delay before continuing
+
+            }
         });
     });
-    // Add a delay (e.g., 10 seconds) before continuing
-    setTimeout(() => {
-        // Wait for 10 seconds before clicking the 'Create Dub' button so that tokens are calculated
-        const createDubButton = document.querySelector(".btn.btn-primary.btn-md.btn-normal");
-
-        if (createDubButton) {
-            // Click on the 'Create Dub' button
-            createDubButton.click();
-        } else {
-            console.error('Create Dub button not found.');
-        }
-    }, 10000); // 10 second delay before clicking 'Create Dub'
-
-
 }
